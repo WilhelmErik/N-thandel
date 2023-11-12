237fd700da8b59e1a770801d605e1e1fa3b0d811
Albin Lundström <albin.wilhelm.erik@gmail.com>
2023-11-12 15:25:53 +0100

    changed img names

--- Backend/controllers/products-controller.js
+++ Backend/controllers/products-controller.js
@@ -1,8 +1,5 @@
 import fetchProducts from "../utils/fetchData.js";
 
-const BASE_URL =
-  "https://my-json-server.typicode.com/WilhelmErik/N-thandel/testProducts/";
-
 const listProducts = async (req, res) => {
   const response = await fetchProducts();
   res.status(200).json(response);
@@ -16,12 +13,10 @@ const getProduct = async (req, res) => {
     return;
   }
 
-  const id = req.params.id;
+  const id = +req.params.id;
   const response = await fetchProducts();
-  console.log(response, " What is the shape");
   const targetProduct = response.find((product) => product.id === id);
-  console.log(targetProduct);
-  res.status(200).json(response);
+  res.status(200).json(targetProduct);
 };
 
 export { listProducts, getProduct };
--- Backend/utils/fetchData.js
+++ Backend/utils/fetchData.js
@@ -1,7 +1,8 @@
+const BASE_URL =
+  "https://my-json-server.typicode.com/WilhelmErik/N-thandel/testProducts/";
 export default async function fetchProducts() {
-    console.log("Fetching in progress");
-    const response = await fetch(BASE_URL);
-    const data = await response.json();
-    console.log(data);
-    return data;
-  }
+  console.log("Fetching in progress");
+  const response = await fetch(BASE_URL);
+  const data = await response.json();
+  return data;
+}
